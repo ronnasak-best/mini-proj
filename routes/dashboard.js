102d3f257188a95e790bdbb1b2e12580915297ab
ronnasak-best <Ronnasak_best@hotmail.com>
2022-02-14 19:14:02 +0700

    dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Product = require('../models/products')
 
 
-router.get('/', (req, res, next) => {
-   
-    res.render('index')
+router.get('/', async (req, res, next) => {
+    const products = await Product.find({quantity: {$lt: 50,$gt:0 }}).count()
+    const products_out_of_stock = await Product.find({quantity: {$eq: 0 }}).count()
+    const products_enable = await Product.find({status: 1 }).count()
+    const products_disable = await Product.find({status: 0 }).count()
+    res.render('index', {products,products_out_of_stock,products_enable,products_disable}) 
 })
 
 module.exports =router
